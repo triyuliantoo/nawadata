b6487624c64c376f60d49da18561a7a1b005c486
triyulianto <triyuliantoo@live.com>
2022-02-14 14:57:29 +0700

    logic test

---
+++ nawadata1/nawadata1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace nawadata1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Input one of line words: ");
+            string words = Console.ReadLine().ToLower();
+            var charsToRemove = new string[] { "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z" };
+            foreach (var c in charsToRemove)
+            {
+                words = words.Replace(c, string.Empty);
+            }
+            char[] sortedT = words.ToCharArray();
+            Array.Sort(sortedT);
+            Console.WriteLine(sortedT);
+        }
+    }
+}
---
+++ nawadata2/nawadata2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace nawadata2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Input the number of families: ");
+            int fams = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Input the number of members in the family: ");
+            string members = Console.ReadLine().ToLower();
+            string trimmed = Regex.Replace(members, @"^[A-Za-z]+", "");
+            string[] CountPassangers = trimmed.Split(' ');
+
+            if(fams != CountPassangers.Length)
+                Console.WriteLine("Input must be equal with count of family");
+            else
+            {
+                decimal total = 0;
+                foreach(var data in CountPassangers)
+                {
+                    total += Convert.ToInt32(data);
+                }
+
+                total /= 4;
+
+                total = Math.Ceiling(total);
+
+                Console.WriteLine(total);
+            }
+        }
+    }
+}
